09fe9b6441581fdc0d1afcd5f05c4e5e9114a253
Charles-indigo21 <charles.verdadero@indigo21.com>
2026-01-26 04:23:28 +0800

    Update CostPlanItem.php

--- app/Models/CostPlanItem.php
+++ app/Models/CostPlanItem.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class CostPlanItem extends Model
 {
-    //
+    public function supplier(): HasOne{
+        return $this->hasOne(Supplier::class, "supplier_id");
+    }
 }
